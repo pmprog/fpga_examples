a271ff0a251e17a5cdd09ce25e0dd6d584d264f4
Marq Watkin <devhead@pmprog.co.uk>
2016-03-07 12:38:14 +0000

    gnfgn

--- quartus/chameleon/simple_start/simple_button_to_led.vhd
+++ quartus/chameleon/simple_start/simple_button_to_led.vhd
@@ -4,11 +4,13 @@ use IEEE.numeric_std.ALL;
 
 -- -----------------------------------------------------------------------
 
-entity simple_start is
+entity simple_button_to_led is
 	port (
 		clk : in std_logic;
 
-		button_one : out std_logic;
-		button_two : out std_logic
+		button_one : in std_logic;
+		button_two : in std_logic;
+		led_one : out std_logic;
+		led_two : out std_logic
 	);
 end entity;
